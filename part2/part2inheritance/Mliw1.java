59fdedc1df63761961b2193f97a5bffff7f84ef7
Balamurugan <balamurugan.csekar@gmail.com>
2026-09-20 23:29:46 +0530

    New Code Added

--- part2/part2inheritance/Mliw1.java
+++ part2/part2inheritance/Mliw1.java
@@ -66,3 +66,70 @@ public class Mliw1 {
 	
 
 }
+// End of Program No 1
+
+
+// Program No:2 is Multi-level Inheritance
+class vehicle
+{
+	String Brand;
+	String Model;
+	
+	vehicle(String Ba,String Mo)
+	{
+		this.Brand=Ba;
+		this.Model=Mo;
+		
+	}
+	void displayvehicle()
+	{
+		System.out.println("Vehicle Brand :"+Brand);
+		System.out.println("Vehicle Model :"+Model);
+	}
+	
+	
+}
+class car extends vehicle
+{
+	int Door;
+	
+	car(String Ba,String Mo,int Do)
+	{
+		super(Ba,Mo);
+		this.Door=Do;
+	}
+	void displaycar()
+	{
+		System.out.println("No of Door :"+Door);
+	}
+}
+class ElectricCar extends car
+{
+	int Capacity;
+	int km;
+	
+	ElectricCar(String Ba,String Mo,int Do,int Ca,int Km)
+	{
+		super(Ba,Mo,Do);
+		this.Capacity=Ca;
+		this.km=Km;
+		
+	}
+	void displayEV()
+	{
+	System.out.println("Battery Capacity :"+Capacity);
+	System.out.println("Total KM Cover :"+km);
+	}
+	
+}
+class Execution
+{
+	public static void main(String [] args)
+	{
+		ElectricCar e1=new ElectricCar("TATA", "Nexon EV", 5, 40, 400);
+		e1.displayvehicle();
+		e1.displaycar();
+		e1.displayEV();
+	}
+}
+// End
