14f793ac09648a1d6c57db3b36f0300d0b008dd1
Balamurugan <balamurugan.csekar@gmail.com>
2026-09-25 08:17:12 +0530

    Update HieInW.java

--- part2/part2inheritance/HieInW.java
+++ part2/part2inheritance/HieInW.java
@@ -76,3 +76,87 @@ public class HieInW {
 	}
 }
 // End
+// Program No: 2
+//start
+class Vehicle
+{
+	String Brand;
+	int  Speed;
+	
+	Vehicle(String Br,int Sp)
+	{
+		this.Brand=Br;
+		this.Speed=Sp;
+	}
+	void displayInfo()
+	{
+		System.out.println("Brand : "+Brand);
+		System.out.println("Speed : "+Speed);
+		
+	}
+	
+}
+class Car extends Vehicle
+{
+	int Doors;
+	
+	Car(String Br,int Sp,int Dor)
+	{
+		super(Br,Sp);
+		this.Doors=Dor;
+	}
+	
+	void displayDrive()
+	{
+		System.out.println(Brand+"car is driving with :"+Doors);
+		
+	}
+}
+class Bike extends Vehicle
+{
+	String Type;
+	Bike(String Br,int Sp,String Ty)
+	{
+		super(Br,Sp);
+		this.Type=Ty;
+	}
+	void displayRide()
+	{
+		System.out.println(Brand+" is a "+Type+" Bike");
+	}
+}
+class Truck extends Vehicle
+{
+	int LoadCapacity;
+	
+	Truck(String Br,int Sp,int LD)
+	{
+		super(Br,Sp);
+		this.LoadCapacity=LD;
+		
+	}
+	void discarryLoad()
+	{
+		System.out.println("Tata truck is carrying a load of "+LoadCapacity);
+	}
+}
+class Program2
+{
+	public static void main(String [] args)
+	{
+		Car c1=new Car("Toyota", 180, 4);
+		Bike b1=new Bike("Pulsar",110,"Sports");
+		Truck t1=new Truck("Tata",80,50);
+		//car Functions call
+		c1.displayInfo();
+		c1.displayDrive();
+		//Bike Function Call
+		b1.displayInfo();
+		b1.displayRide();
+		//Truck Function Call
+		t1.displayInfo();
+		t1.discarryLoad();
+		
+	}
+}
+// End
